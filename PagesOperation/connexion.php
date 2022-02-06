e9cd3cd25f667a90f7a9a1d30a21548d54fb9fb4
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-02-06 14:24:26 +0100

    modefication sur page connection

--- PagesOperation/connexion.php
+++ PagesOperation/connexion.php
@@ -1,6 +1,6 @@
 <?php
  try {
-    $mysql = new PDO("mysql:host=localhost;dbname=e_classe_db", 'root', 'afraklaab99');
+    $mysql = new PDO("mysql:host=127.0.0.1;dbname=e_classe_db", 'root', 'afraklaab99');
 } catch (PDOException $e) {
     echo "Failed :" . $e->getMessage();
 }
